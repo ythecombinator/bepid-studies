d64745be11b9242474b6ab681b7437af4b0b61e1
Matheus Brasil <matheus.brasil10@gmail.com>
2015-08-29 03:09:37 -0300

    Update hello world

--- material/linguagem-C/exemplos/hello.c
+++ material/linguagem-C/exemplos/hello.c
@@ -1,9 +1,11 @@
-/* Hello Darkness, my old friend */
+/* Exemplo de programa estilo "Hello World" na linguagem C */
 
+/* Incluindo a biblioteca padrão de funções de I/O */
 #include <stdio.h>
 
+/* Função principal */
 int main()
 {
-    printf("Hello Darknes, my old friend.\n");
+    printf("Se a vida e um deserto; a minha e o Saara. \n"); /* Mensagem a ser impressa */
     return 0;
 }
