b5d74536b72b932c210e6b8713ff0116e3a774f1
Matheus Brasil <matheus.brasil10@gmail.com>
2015-09-01 11:47:25 -0300

    Add first example of saying odd or even

---
+++ material/linguagem-C/exemplos/impar-ou-par/exemplo1.c
@@ -0,0 +1,23 @@
+/* Exemplo de programa que pede um número ao usuário e verifica se ele é impar ou par */
+
+/* Incluindo a biblioteca padrão de funções de I/O */
+#include <stdio.h>
+
+/* Função principal */
+int main()
+{
+  /* Declaramos uma variável para guardar o número digitado */
+  int num;
+
+  printf("Qual o valor? \n");
+  scanf("%i", &num);
+
+  /* Se um número é par, ele é divisível por 2; caso contrário, ele é ímpar */
+  if (num % 2 == 0)
+    printf("Par \n");
+  else
+    printf("Impar \n");
+
+  return 0;
+}
+
