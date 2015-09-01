7c4d5d17ec8eee09efbb1b5c06a4b3f9ff00b2ff
Matheus Brasil <matheus.brasil10@gmail.com>
2015-09-01 20:21:11 -0300

    Update first addition example to perform multiple basic operations

--- material/linguagem-C/exemplos/somar/exemplo1.c
+++ material/linguagem-C/exemplos/somar/exemplo1.c
@@ -1,4 +1,4 @@
-/* Exemplo de programa que pede dois números ao usuário e soma eles */
+/* Exemplo de programa que pede dois números ao usuário e faz as quatro operações aritméticas */
 
 /* Incluindo a biblioteca padrão de funções de I/O */
 #include <stdio.h>
@@ -6,39 +6,31 @@
 /* Função principal */
 int main()
 {
+  /* Declarando variáveis do tipo int para serem os operandos */
+  int num1, num2;
+  /* Declarando variáveis para serem os resultados das operações */
+  int soma, diferenca, produto;
+  /* Essa é do tipo float, pois o resultado da divisão não necessariamente é um número inteiro */
+  float quociente;
 
-  /* Declarando variáveis do tipo int para serem os operandos e o resultado*/
-  int num1, num2, resultado;
-  /* Declarando uma variável do tipo char para ser a resposta do usuário
-  quando questionado se deseja realizar outra soma */
-  char resposta;
+  /* Pede dois números */
+  printf("Digite dois valores inteiros: \n");
+  /* Recebe os números */
+  scanf("%d%d", &num1, &num2);
 
-  /* Laço ("infinito") que performa a operação de soma enquanto o usuário desejar  */
-  while (1) {
-    /* Pede dois números */
-    printf("Digite dois valores inteiros: \n");
-    /* Recebe os números */
-    scanf("%d%d", &num1, &num2);
-    getchar();
+  /* Atribui a cada variável o resultado da respectiva operação*/
+  soma = num1 + num2;
+  diferenca = num1 - num2;
+  produto = num1 * num2;
+  /* É válido a leitura da seção sobre o operador cast para entender o que ocorre aqui.
+  (https://github.com/mabrasil/bepid-studies/blob/master/material/linguagem-C/sintaxe.md#cast) */
+  quociente = num1 / (float)num2;
 
-    /* Soma os resultados */
-    resultado = num1 + num2;
-
-    /* Imrime a resposta */
-    printf("Resposta: %d. \n", resultado);
-
-    /* Pergunta se o usuário quer fazer outra operação */
-    printf("Quer realizar outra soma? (s/n)\n");
-    /* Lê a resposta do usário */
-    scanf("%c", &resposta);
-
-    /* Se o usuário tiver colocado sim, continua no loop (e passa pra próxima soma) */
-    if (resposta == 's' || resposta == 'S')
-       continue;
-    /* Se não, para o loop */
-    else
-      break;
-  }
+  /* Imprime os resultados */
+  printf("Soma: %d\n",soma);
+  printf("Diferenca: %d\n",diferenca);
+  printf("Produto = %d\n",produto);
+  printf("Quociente = %.2f\n",quociente);
 
   return 0;
 }
