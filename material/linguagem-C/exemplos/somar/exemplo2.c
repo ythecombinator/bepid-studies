0e7ed93c7b8daeba5645535da1b6a6d0bd37fe5b
Matheus Brasil <matheus.brasil10@gmail.com>
2015-09-02 01:13:13 -0300

    Update second basic operations example

--- material/linguagem-C/exemplos/somar/exemplo2.c
+++ material/linguagem-C/exemplos/somar/exemplo2.c
@@ -3,12 +3,51 @@
 /* Incluindo a biblioteca padrão de funções de I/O */
 #include<stdio.h>
 
-long somar(long, long);
+/* Função para realizar a soma */
+long somar(long n1, long n2)
+{
+  long resultadoSoma;
+
+  resultadoSoma = n1 + n2;
+
+  return resultadoSoma;
+}
+
+/* Função para realizar a subtração */
+long subtrair(long n1, long n2)
+{
+  long resultadoDiferenca;
+
+  resultadoDiferenca = n1 - n2;
+
+  return resultadoDiferenca;
+}
+
+/* Função para realizar a multiplicação */
+long multiplicar(long n1, long n2)
+{
+  long resultadoProduto;
+
+  resultadoProduto = n1 * n2;
+
+  return resultadoProduto;
+}
+
+/* Função para realizar a divisão */
+float dividir(float n1, float n2)
+{
+  float resultadoDiferenca;
+
+  resultadoDiferenca = n1 / n2;
+
+  return resultadoDiferenca;
+}
 
 /* Função principal */
 int main()
 {
-  long num1, num2, resultado;
+  long num1, num2, soma, diferenca, produto;
+  float quociente;
 
   /* Pede dois números */
   printf("Digite dois valores inteiros: \n");
@@ -17,20 +56,16 @@ int main()
   scanf("%ld%ld", &num1, &num2);
 
   /* Soma os resultados (por meio da função "somar") */
-  resultado = somar(num1, num2);
+  soma = somar(num1, num2);
+  diferenca = subtrair(num1, num2);
+  produto = multiplicar(num1, num2);
+  quociente = dividir(num1, num2);
 
-  /* Imprime a resposta */
-  printf("Resposta: %ld \n", resultado);
+  /* Imprime os resultados */
+  printf("Soma: %d\n",soma);
+  printf("Diferenca: %d\n",diferenca);
+  printf("Produto = %d\n",produto);
+  printf("Quociente = %.2f\n",quociente);
 
   return 0;
-}
-
-/* Função para realizar a soma */
-long somar(long n1, long n2)
-{
-  long soma;
-
-  soma = n1 + n2;
-
-  return soma;
 }
